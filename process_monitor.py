5a582b1d27627d51a5a3f6bd7dae22247972fcce
parhta2000 <parthaprateem2000@gmail.com>
2021-11-15 07:30:31 +0530

    added monitoring for separate processess

--- process_monitor.py
+++ process_monitor.py
@@ -2,7 +2,10 @@ from tqdm import tqdm
 from time import sleep
 import psutil
 
-
+process_id = 1
+for process_id in psutil.pids()[-10:]:
+    process = psutil.Process(process_id)
+    print("PID #",process.name()," RAM = ",round(process.memory_percent(),2)," CPU = ",process.cpu_percent())
 
 with tqdm(total=100, desc='cpu%', position=1) as cpubar, tqdm(total=100, desc='ram%', position=0) as rambar:
     while True:
